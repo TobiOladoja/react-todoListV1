331ef4ce3af93f0cde95ff51f655e0bbe1a55253
longtermtobi <tbfullstack@gmail.com>
2019-12-27 12:55:24 +0000

    Got it to basic working stage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' }
+    ]
+  };
   render() {
-    return <div className='App'></div>;
+    return (
+      <div className='App'>
+        <h1 className='center blue-text'>Todo's</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
   }
 }
 
