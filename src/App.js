a64919c82ab0067a98e812bcc88c54e6a5fd6dea
longtermtobi <tbfullstack@gmail.com>
2019-12-27 13:19:10 +0000

    Added deleteToDo function, currently testing it with console log

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ class App extends Component {
       { id: 2, content: 'play mario kart' }
     ]
   };
+  // Hook up an event to delete events, you need to add the function to delete to be here.
+  // Because it has to interact with the state above. So function created below
+  deleteTodo = id => {
+    // function going to take id as param so we can look up and filter
+    console.log(id);
+  };
   render() {
     return (
       <div className='todo-app container'>
         <h1 className='center blue-text'>Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
